f035fa047030d26b0acc9233637c8a5d336451b9
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-05-04 19:36:47 +0530

    Update video.py

--- Mangandi/video.py
+++ Mangandi/video.py
@@ -2,7 +2,7 @@ import requests
 from typing import Union
 
 class VideoUploader:
-    URL = "https://mangandi-2-0.onrender.com"
+    URL = "https://storage.mangoi.in"
 
     def __init__(self, file_path: str):
         self.file_path = file_path
@@ -15,7 +15,7 @@ class VideoUploader:
         data = self._read_file()
         files = {"file": data}
         response = requests.post(
-            f"{self.URL}/vupload",
+            f"{self.URL}/upload",
             files=files
         )
 
@@ -26,7 +26,7 @@ class VideoUploader:
 
     def upload(self) -> str:
         response_json = self._upload()
-        if "link" in response_json:
-            return response_json["link"]
+        if "url" in response_json:
+            return response_json["url"]
         else:
             return "not found"
